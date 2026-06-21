15161f0ac8dbc8849c7f3bb15302684276b366ca
kill3rb0ii <kill3rb0ii.studios@gmail.com>
2026-06-21 17:59:27 +0530

    basic movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,21 +4,38 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
+    private Vector2 moveInput;
+    [SerializeField] private int moveSpeed = 100;
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    public void jump(InputAction.CallbackContext context)
+    public void Jump(InputAction.CallbackContext context)
     {
         if (context.started)
         {
-            Debug.Log(context);
             rb.AddForce(0, 100, 0);
-            
         }
         
     }
 
+    public void Move(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            moveInput = context.ReadValue<Vector2>();
+        }
+        else if (context.canceled)
+        {
+            moveInput = Vector2.zero;
+        }
+    }
+
+
+    private void Update()
+    {
+        rb.linearVelocity = new Vector3(moveInput.x*moveSpeed*Time.deltaTime, rb.linearVelocity.y, moveInput.y*moveSpeed*Time.deltaTime);
+    }
 
 }
